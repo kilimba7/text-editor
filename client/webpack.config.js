dd73ee825b622150608713eee3dd29277f84cac0
Kilimba <dyaul003@umn.edu>
2022-09-15 22:35:49 -0500

    need to rescope my service worker, but I fixed the stale error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,12 +20,12 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plug',
+        title: 'Webpack Plugin',
       }),
 
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'service-worker.js'
+        swDest: 'src-sw.js',
       }),
 
       new WebpackPwaManifest({
